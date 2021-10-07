54304e626685425717eb5a3a89bd5a094e55b6f8
Lingors <maks.26rus1997@mail.ru>
2021-10-07 21:29:08 +0300

    Add files via upload

---
+++ src/Loader/Loader.js
@@ -0,0 +1,19 @@
+import "./Loader.css"
+import React from "react";
+
+export default function Loader() {
+    return (
+        <div>
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
